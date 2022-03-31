db8c8daf22d53c750e45ae52592b9a45683e9346
babboe1 <ayodelaustinab@gmail.com>
2022-03-31 14:31:56 +0100

    feat: create BackDrop Function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,8 @@
 const BACKDROP = document.getElementById('backdrop');
 const MENU_BTN = document.querySelector('.MenuBtn');
 const CLOSE_BTN = document.getElementById('close-btn');
+
+const BackDrop = () => {
+   BACKDROP.classList.toggle('visible');
+};
+
