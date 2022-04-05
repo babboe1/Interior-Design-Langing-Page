1acc5f194c0aa24085e73d97112aba780a4cc91f
babboe1 <ayodelaustinab@gmail.com>
2022-04-05 16:11:17 +0100

    refactor: format code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -37,7 +37,7 @@ const MenuOptionOut = () => {
 };
 const clearInput = () => {
 	USER_INPUT.value = '';
-   ERROR_TEXT.classList.remove('visible');
+	ERROR_TEXT.classList.remove('visible');
 };
 
 const emailValidationHandler = () => {
